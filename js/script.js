bce9702fa59140b0e401ad40a11841ace254d5ed
AntonelloStefania <antonelloste94@gmail.com>
2023-05-17 15:33:34 +0200

    [fix] limitati  i decimali. completo

--- js/script.js
+++ js/script.js
@@ -5,19 +5,20 @@ let sconto = ((biglietto_intero / 5));
 let sconto_old = (((biglietto_intero / 10) * 4));
 let biglietto_scontato = (biglietto_intero - sconto);
 let biglietto_scontato_old = (biglietto_intero - sconto_old);
+let tot_min = biglietto_scontato.toFixed(2);
+let tot_old = biglietto_scontato_old.toFixed(2);
+let tot = biglietto_intero.toFixed(2);
 
 
 let eta = parseInt(prompt('Quanti anni hai?').trim());
 if (eta < 18) {
-    let sconto = ((biglietto_intero / 5));
     let biglietto_scontato = (biglietto_intero - sconto);
-    document.getElementById("prezzo_biglietto").innerHTML = `${biglietto_scontato} €`;
-    } else if (eta >= 65) {
-    let sconto_old = (((biglietto_intero / 10) * 4));
+    document.getElementById("prezzo_biglietto").innerHTML = `${tot_min} €`;
+} else if (eta >= 65) {
     let biglietto_scontato_old = (biglietto_intero - sconto_old);
-    document.getElementById("prezzo_biglietto").innerHTML = `${biglietto_scontato_old} €`;
+    document.getElementById("prezzo_biglietto").innerHTML = `${tot_old} €`;
 } else {
-    document.getElementById("prezzo_biglietto").innerHTML = `${biglietto_intero} €`;
+    document.getElementById("prezzo_biglietto").innerHTML = `${tot} €`;
 }
 
 
