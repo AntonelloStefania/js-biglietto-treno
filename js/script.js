2790f38ce16a897cf84455852bb27ed72ce90ad5
AntonelloStefania <antonelloste94@gmail.com>
2023-05-17 15:21:47 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,33 +1,25 @@
 let km_utente = parseInt(prompt('Quanti km devi percorrere?').trim());
-console.log(km_utente,typeof km_utente)
 const tariffa = 0.21;
 let biglietto_intero =  km_utente * tariffa;
-console.log(biglietto_intero)
-let sconto;
-console.log(sconto)
-//let sconto_old;
-//console.log(sconto_old)
-let biglietto_scontato;
-console.log (biglietto_scontato)
+let sconto = ((biglietto_intero / 5));
+let sconto_old = (((biglietto_intero / 10) * 4));
+let biglietto_scontato = (biglietto_intero - sconto);
+let biglietto_scontato_old = (biglietto_intero - sconto_old);
 
 
 let eta = parseInt(prompt('Quanti anni hai?').trim());
 if (eta < 18) {
     let sconto = ((biglietto_intero / 5));
-    console.log(sconto);
     let biglietto_scontato = (biglietto_intero - sconto);
-    console.log(biglietto_scontato);
+    document.getElementById("prezzo_biglietto").innerHTML = `${biglietto_scontato} €`;
     } else if (eta >= 65) {
-    let sconto = (((biglietto_intero / 10) * 4));
-    console.log(sconto);
-    let biglietto_scontato = (biglietto_intero - sconto);
-    console.log(biglietto_scontato);
+    let sconto_old = (((biglietto_intero / 10) * 4));
+    let biglietto_scontato_old = (biglietto_intero - sconto_old);
+    document.getElementById("prezzo_biglietto").innerHTML = `${biglietto_scontato_old} €`;
 } else {
-    let biglietto_intero = biglietto_scontato;
-    console.log(biglietto_intero);
+    document.getElementById("prezzo_biglietto").innerHTML = `${biglietto_intero} €`;
 }
 
-document.getElementById("prezzo_biglietto").innerHTML = biglietto_scontato
 
 
 
@@ -35,4 +27,3 @@ document.getElementById("prezzo_biglietto").innerHTML = biglietto_scontato
 
 
 
-console.log(biglietto_scontato_old);
